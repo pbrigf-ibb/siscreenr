a202d6a9cc4e02cf24ed8a80be2cfbd52d13916e
olobiolo <olobiolo@gmail.com>
2019-04-23 17:21:09 +0200

    add function: zscore

---
+++ R/fun-zscore.R
@@ -0,0 +1,86 @@
+#' calculate (robust) zscore
+#'
+#' Given a numeric vector, convert the numbers to z scores or robust z scores.
+#' The function accepts raw values as well as normalized ones.
+#'
+#' The z score is a data point standardized to the distribution it comes from.
+#' It is a measure of the deviation of the point from the distribution's
+#' location parameter expressed in terms of the dispersion parameter of that distribution.
+#'
+#' For a normal z score the difference between a point and the distribution mean
+#' is divided by the distribution's standard deviation. For a robust z score
+#' the mean and standard deviation are replaced by the median and median absolute deviation,
+#' respectively.
+#'
+#' @param x a numeric vector
+#' @param robust logical flag whether to calculate normal or robust z scores
+#' @param deviations logical flag whether the supplied data is raw or normalized
+#' @param reference logical vector that points out reference observations
+#'
+#' @return a numeric vector of z scores
+#'
+#' @section Deviations:
+#' Data is accepted as raw or normalized and the function can be informed of this with a logical flag.
+#' For deviations the location parameter is assumed to be 0.
+#' If normalization is done by simply subtracting the mean/median of the whole sample
+#' from each data point, this is redundant but other normalization methods
+#' may introduce differences differences.
+
+#' @section Reference:
+#' If desired, data points in the sample can be standardized against part of the distribution
+#' rather than the whole. Should this be the case, supply a logical vector to determine
+#' the reference subpopulation.
+#'
+#' @examples
+#' a <- rnorm(1000, 55, 3)
+#' hist(a, breaks = 50)
+#' z <- zscore(a)
+#' hist(z, breaks = 50)
+#' plot(x = a, y = z)
+
+zscore <- function(x, robust = TRUE, deviations = FALSE, reference = NULL) {
+  if (!is.numeric(x)) stop('"x" must be numeric')
+
+  if (is.null(reference)) {
+    ref <- x
+  } else {
+    ref <- x[reference]
+  }
+  if (deviations == TRUE) {
+    loc <- 0
+  } else if (robust == TRUE) {
+    loc <- median(ref, na.rm = TRUE)
+  } else if (robust == FALSE) {
+    loc <- mean(ref, na.rm = TRUE)
+  }
+  if (robust == TRUE) {
+    disp <- mad(ref, na.rm = TRUE)
+  } else if (robust == FALSE) {
+    disp <- sd(ref, na.rm = TRUE)
+  }
+
+  # alternative syntax
+  # ref <-
+  #   if (is.null(reference)) {
+  #     x
+  #   } else {
+  #     x[reference]
+  #   }
+  # loc <-
+  #   if (deviations) {
+  #     0
+  #   } else if (robust) {
+  #     median(ref, na.rm = TRUE)
+  #   } else {
+  #     mean(ref, na.rm = TRUE)
+  #   }
+  # disp <-
+  #   if (robust) {
+  #     mad(ref, na.rm = TRUE)
+  #   } else {
+  #     mean(ref, na.rm = TRUE)
+  #   }
+
+  zsc <- (x - loc) / disp
+  return(zsc)
+}
