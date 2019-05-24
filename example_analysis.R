7c3214f6808c1d5f8e3d894a99e221e924d5e819
olobiolo <olobiolo@gmail.com>
2019-05-24 13:26:08 +0200

    updated example analysis

--- example_analysis.R
+++ example_analysis.R
@@ -1,47 +1,52 @@
-#library(siscreenr)
+
 library(magrittr)
 library(dplyr)
 library(tidyr)
 library(lubridate)
 library(ggplot2)
+library(data.table)
+library(acutils)
+library(siscreenr)
 
 # go to screen directory
 .home <- setwd('C:/Users/Olo/Desktop/R/ground/')
 .home <- setwd('C:/Users/Olek/Desktop/R works/ground/')
 
-plot_screen_progress(file = NULL)
-layouts('layout_S01_test.txt', 'layout_S01_control.txt', file = 'layout_S01.txt')
+ann <- data.table::fread('ANNOTATION.LIBRARY.GENOMIC_20181108.txt')
+
+siscreenr::plot_screen_progress('C:/Users/Olek/Desktop/R works/ground/', file = NULL)
+#layouts('layout_S01_test.txt', 'layout_S01_control.txt', file = 'layout_S01.txt')
 
 s <-
-  build_screen('screenlog_S01.txt', 'layout_S01.txt', rem.col = c(0,2), zero.to.NA = T) %>% dplyr::glimpse() %>%
-  clean_column_names() %>% dplyr::glimpse() %>%
-  dplyr::mutate(position = insert_zeros(position)) %>% dplyr::glimpse()
+  build_screen('screenlog_S01.txt', 'layout_S01.txt', rem.col = c(0,2), zero.to.NA = T) %>%
+  acutils::clean_column_names() %>%
+  dplyr::mutate(position = insert_zeros(position))
 
 setwd(.home)
 
-s %<>% mutate(green_fraction = nucs_in_green / (nucs_in_green + nucs_in_red))
+s %<>% dplyr::mutate(green_fraction = nucs_in_green / (nucs_in_green + nucs_in_red))
 
 vs <- c('nucs_in_green', 'nucs_in_red', 'green_fraction')
 vn <- paste(vs, 'normalized_medpolish', sep = '_')
 vz <- paste(vn, 'zscore', sep = '_')
+vh <- paste(vz, 'hitscore', sep = '_')
 
-n <- s %>% group_by(plate, replica, plated) %>%
-  normalize(variables = vs, method = 'medpolish') %>%
-  dplyr::glimpse()
+n <- s %>% dplyr::group_by(plate, replica, plated) %>% normalize(variables = vs, method = 'medpolish')
 
-R <- s %>% slice(1:max(.$well)) %>% .$well_type %>% grepl('sample', .); matrix(R, 16, 24)
+z <- n %>% dplyr::group_by(plate_type) %>% siscreenr::zscore(dev = T, ref = well_type == 'sample', var = vn)
 
-z <- n %>% dplyr::mutate_at(vn, dplyr::funs(zscore = zscore(., reference = R))) %>% dplyr::glimpse()
+stop('just stop')
 
 TRESHOLD <- 2
-h <- z %>% dplyr::mutate_at(vz, dplyr::funs(hitscore = hitscore(., TRESHOLD))) %>% dplyr::glimpse()
+h <- z %>% dplyr::ungroup() %>%
+  dplyr::mutate_at(.vars = vz, .funs = list(hitscore = ~hitscore(., TRESHOLD))) %>% dplyr::glimpse()
 
+r <- h %>% dplyr::group_by(plate, plated, imaged, well) %>%
+  dplyr::mutate_at(vh, .funs = list(sumhitscore = sum)) %>% dplyr::glimpse()
+
+r <- h %>% dplyr::group_by(plate, plated, imaged, well) %>%
+  dplyr::mutate_at(vh, .funs = list(
+    frachitscore = function(x) {sum(x, na.rm = T) / length(x[!is.na(x)])})) %>% dplyr::glimpse()
 
 
 stop('=== D = O = N = E ===')
-
-microbenchmark::microbenchmark(
-  sub = build_screen('screenlog_S01.txt', 'layout_S01.txt', rem.col = c(0,2)),
-  conv = build_screen2('screenlog_S01.txt', 'layout_S01.txt', rem.col = c(0,2)),
-  times = 10
-  )
